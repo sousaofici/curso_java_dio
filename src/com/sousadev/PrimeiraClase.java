bdeb51cdb23c513b4afeccb3c64066ab5a5a4e70
mauricio de sousa <sousaofici.dev@gmail.com>
2022-01-20 16:14:35 -0300

    first commit

---
+++ src/com/sousadev/PrimeiraClase.java
@@ -0,0 +1,15 @@
+package com.sousadev;
+
+import com.sousadev.model.Gato;
+
+public class PrimeiraClase {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a= 16;
+        int b = 34;
+        int soma = a + b ;
+        System.out.println("a soma entre :" +a+"+"+b+ "\ne igaula a :"+ soma);*/
+    }
+
+}
